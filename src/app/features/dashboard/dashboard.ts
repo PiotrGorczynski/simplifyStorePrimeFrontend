bbb2ac4d945c433d85c560ce5261a23ae4f5fd15
Piotr Gorczyński <p.gorczynski4@gmail.com>
2026-01-03 23:47:24 +0100

    Added basics for dashboard

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
-  template: `<h1>Dashboard</h1>`,
-  styles: []
+  imports: [CommonModule, TableModule, ButtonModule, CardModule, TagModule],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.scss'
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  products: any[] = [];
+
+  ngOnInit() {
+    this.products = [
+      { id: 4, info: 'New Info 4', salesOrder: 'service', invoices: 'yes', category: 'retail chains' },
+      { id: 5, info: 'New Info 5', salesOrder: 'bulk', invoices: 'no', category: 'individual' },
+      { id: 54, info: 'info 5', salesOrder: 'order', invoices: 'yes', category: 'business' },
+      { id: 6, info: 'New info 6', salesOrder: 'bulk', invoices: 'no', category: 'business' },
+      { id: 7, info: 'New info 7', salesOrder: 'service', invoices: 'yes', category: 'detail' }
+    ];
+  }
+}
