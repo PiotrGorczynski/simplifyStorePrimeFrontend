8525061379f00a90f08bb056f1c1cfac96c0b10b
Piotr Gorczyński <p.gorczynski4@gmail.com>
2026-01-04 23:51:54 +0100

    Dashboard and sidebar fixes + added logo

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -21,6 +21,14 @@ export class DashboardComponent implements OnInit {
       { id: 5, info: 'New Info 5', salesOrder: 'bulk', invoices: 'no', category: 'individual' },
       { id: 54, info: 'info 5', salesOrder: 'order', invoices: 'yes', category: 'business' },
       { id: 6, info: 'New info 6', salesOrder: 'bulk', invoices: 'no', category: 'business' },
+      { id: 4, info: 'New Info 4', salesOrder: 'service', invoices: 'yes', category: 'retail chains' },
+      { id: 5, info: 'New Info 5', salesOrder: 'bulk', invoices: 'no', category: 'individual' },
+      { id: 54, info: 'info 5', salesOrder: 'order', invoices: 'yes', category: 'business' },
+      { id: 6, info: 'New info 6', salesOrder: 'bulk', invoices: 'no', category: 'business' },
+      { id: 4, info: 'New Info 4', salesOrder: 'service', invoices: 'yes', category: 'retail chains' },
+      { id: 5, info: 'New Info 5', salesOrder: 'bulk', invoices: 'no', category: 'individual' },
+      { id: 54, info: 'info 5', salesOrder: 'order', invoices: 'yes', category: 'business' },
+      { id: 6, info: 'New info 6', salesOrder: 'bulk', invoices: 'no', category: 'business' },
       { id: 7, info: 'New info 7', salesOrder: 'service', invoices: 'yes', category: 'detail' }
     ];
   }
